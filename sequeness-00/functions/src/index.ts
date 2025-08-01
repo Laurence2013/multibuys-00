b6e4ded21b2402f10e27fba6300213c29248cdd0
lorenzo187 <lozza244@protonmail.com>
2025-08-01 06:48:47 +0100

    96-commit

--- sequeness-00/functions/src/index.ts
+++ sequeness-00/functions/src/index.ts
@@ -1,19 +1,29 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {setGlobalOptions} from "firebase-functions";
 import {onRequest} from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
+import axios from 'axios';
 
 setGlobalOptions({ maxInstances: 10 });
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from Firebase Functions!");
+});
+export const api = onRequest(async(request, response) => {
+  logger.info('myHttpRequestFunction received a request!');
+  switch (request.method) {
+    case "GET":
+      const response00 = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+      response.send(response00.data);
+      break;
+    case "POST":
+      const body = request.body;
+      response.send(body);
+      break;
+    case "DELETE":
+      response.send('It was DELETE request');
+      break;
+    default:
+      response.send('It was a default request...');
+  }
 });
