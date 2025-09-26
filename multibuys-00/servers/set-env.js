20b463729b1345631fa5079e5f7cc5250760c18e
Laurence2013 <lozza244@protonmail.com>
2025-09-26 21:55:57 +0100

    18-19-commit

---
+++ multibuys-00/servers/set-env.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const fs = require('fs');
+
+const environmentFileContent = `
+export const environment = {
+	production: '${process.env.PRODUCTION}',
+	useEmulators: '${process.env.USE_EMULATORS}',
+	firebaseConfig: {
+		apiKey: '${process.env.API_KEY}',
+		authDomain: '${process.env.AUTH_DOMAIN}',
+		projectId: '${process.env.PROJECT_ID}',
+		storageBucket: '${process.env.STORAGE_BUCKET}',
+		messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+		appId: '${process.env.APP_ID}',
+		measurementId: '${process.env.MEASUREMENT_ID}',
+	}
+};
+`;
+
+fs.writeFileSync('./environments/environment.ts', environmentFileContent);
